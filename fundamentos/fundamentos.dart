59408bd03c9e5e48355532cf0ba42a7d5393e01b
JoseMamede <mamede.jose@academico.ifpb.edu.br>
2026-05-06 13:53:43 +0000

    por_hora_tudo_ok

---
+++ fundamentos/fundamentos.dart
@@ -0,0 +1,36 @@
+void main() {
+    // Questão 1
+    int idade = 42;
+    print('Idade: $idade');
+
+    // Questão 2
+    double altura = 1.62;
+    print('Altura: $altura');
+  
+    // Questão 3
+    String pet = "Rodolfio";
+    print('Nome do pet: $pet');  
+
+    // Questão 4
+    bool isLigada = false;
+    print('A lâmpada está ligada? $isLigada');
+
+    // Questão 5
+    int a = 10;
+    int b = 3;
+    int soma = a + b;
+    print('A soma de $a + $b é: $soma');
+
+    // Questão 6
+    // Usando as variáveis a e b da questão a cima
+    int resto = a % b;
+    print('O resto da divisão de $a por $b é: $resto');
+
+    // Questão 7
+    double valorCompra = 100.0;
+    // Aplica 10% de desconto usando operadores aritméticos
+    double valorFinal = valorCompra - (valorCompra * 0.10);
+    print('Valor original: $valorCompra | Com 10% de desconto: $valorFinal');
+
+
+}
