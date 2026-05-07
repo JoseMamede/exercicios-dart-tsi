7992c6ef17f049114e77b44ee01e5edab4879d09
JoseMamede <mamede.jose@academico.ifpb.edu.br>
2026-05-07 14:12:31 +0000

    menos2

--- estruturas/estruturas.dart
+++ estruturas/estruturas.dart
@@ -1,4 +1,4 @@
-import 'dart:io'; 
+import 'dart:io';
 
 void main() {
   // Questão 16
@@ -13,34 +13,48 @@ void main() {
   }
 
   // Questão 17
-  double nota = 7.5; 
+  double nota = 7.5;
 
   if (nota >= 7) {
-
-  print("Aprovado"); 
+    print("Aprovado");
   } else if (nota >= 5 && nota <= 6.9) {
-  print("Recuperação"); 
+    print("Recuperação");
   } else {
-  print("Reprovado"); 
+    print("Reprovado");
   }
 
   // Questão 18
-  int numero = 17; 
-  String parOuImpar = (numero % 2 == 0) ? "Par" : "Impar";
-  print('Questão 18 - O número $numero é: $parOuImpar');
+  int numero18 = 17;
+  String parOuImpar = (numero18 % 2 == 0) ? "Par" : "Impar";
+  print('Questão 18 - O número $numero18 é: $parOuImpar');
 
   // Questão 19
   int dia = 3;
 
   switch (dia) {
-    case 1: print("Domingo"); break;
-    case 2: print("Segunda-feira"); break;
-    case 3: print("Terça-feira"); break;
-    case 4: print("Quarta-feira"); break;
-    case 5: print("Quinta-feira"); break;
-    case 6: print("Sexta-feira"); break;
-    case 7: print("Sábado"); break;
-    default: print("Dia inválido");
+    case 1:
+      print("Domingo");
+      break;
+    case 2:
+      print("Segunda-feira");
+      break;
+    case 3:
+      print("Terça-feira");
+      break;
+    case 4:
+      print("Quarta-feira");
+      break;
+    case 5:
+      print("Quinta-feira");
+      break;
+    case 6:
+      print("Sexta-feira");
+      break;
+    case 7:
+      print("Sábado");
+      break;
+    default:
+      print("Viajou mano");
   }
 
   // Questão 20
@@ -50,13 +64,63 @@ void main() {
 
   int maior = n1;
 
-if (n2 > maior) {
-  maior = n2;
-}
-if (n3 > maior) {
-  maior = n3;
-}
+  if (n2 > maior) {
+    maior = n2;
+  }
+  if (n3 > maior) {
+    maior = n3;
+  }
 
-print('Questão 20 - O maior número entre $n1, $n2 e $n3 é: $maior');
+  print('O maior número entre $n1, $n2 e $n3 é: $maior');
 
+  // Questão 21
+  for (int i = 10; i >= 1; i--) {
+    print(i);
+  }
+
+  // Questão 22
+  int somaPares = 0;
+
+  for (int i = 1; i <= 100; i++) {
+    if (i % 2 == 0) {
+      somaPares += i;
+    }
+  }
+
+  print('A soma de todos os números pares de 1 a 100 é: $somaPares');
+
+  // Questão 23
+  print("Digite um número para ver a tabuada:");
+
+  String? entradaTabuada = stdin.readLineSync();
+  int numTab = int.parse(entradaTabuada ?? '0');
+
+  print('--- Tabuada do $numTab ---');
+  for (int i = 1; i <= 10; i++) {
+    print('$numTab x $i = ${numTab * i}');
+  }
+
+  // Questão 24
+  int contador = 0;
+  int numero24 = 0;
+
+  print("Digite números (digite um negativo para parar):");
+
+  while (numero24 >= 0) {
+    String? entrada = stdin.readLineSync();
+    numero24 = int.parse(entrada ?? '0');
+
+    if (numero24 >= 0) {
+      contador++;
+    }
+  } 
+
+  // Questão 25
+  List<String> nomes = ['Chico', 'Carlos Prates', 'Rafinha', 'Alex Poatan'];
+
+  print('Nomes em letras maiúsculas:');
+
+  for (var nome in nomes) {
+    print(nome.toUpperCase());
+  }
 }
